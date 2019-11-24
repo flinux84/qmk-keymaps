37b74c4228bd49e6d634133e583af2a91dfea211
flinux84 <linus.soderholm@gmail.com>
2019-11-24 20:20:00 +0100

    Update keymap.c
    jhajaj

--- kbd67mk2rgb/keymap.c
+++ kbd67mk2rgb/keymap.c
@@ -2,12 +2,18 @@
 #define _LAYER0 0
 #define _LAYER1 1
 #define _LAYER2 2
+uint16_t timer = 0;
+bool windows_run = false;
+char code[4] = "code";
+
 
 enum custom_keycodes {
 	VS_FORMAT,
     VS_COMMENT,
     VS_UNCOMMENT,
-    VS_EXTRACTMETHOD
+    VS_EXTRACTMETHOD,
+    RUN_VSCODE,
+    CLEAR
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,8 +24,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_N,    KC_M,  KC_COMM, KC_DOT,   KC_SLSH,  KC_RSFT,           KC_UP,     KC_END,\
 		    KC_LCTL,       KC_LGUI,  KC_LALT,                KC_SPC,                KC_RALT,           MO(1),    KC_LEFT,           KC_DOWN,   KC_RIGHT),
 		[_LAYER1] = LAYOUT_65_ansi_blocker( /* FN1 */
-			KC_GESC,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_HOME,\
-		    KC_TRNS,       KC_TRNS,  KC_TRNS, VS_EXTRACTMETHOD,KC_TRNS, KC_TRNS, KC_TRNS, VS_UNCOMMENT, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, KC_TRNS,    KC_PGUP,\
+			CLEAR,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,   KC_HOME,\
+		    KC_TRNS,       KC_TRNS,  KC_TRNS, VS_EXTRACTMETHOD,KC_TRNS, KC_TILDE, KC_TRNS, VS_UNCOMMENT, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, KC_TRNS,    KC_PGUP,\
 			CTL_T(KC_CAPS),KC_TRNS,  KC_TRNS, KC_TRNS,VS_FORMAT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_TRNS,  KC_PGDN,\
 		    KC_LSFT,       KC_TRNS,  KC_TRNS, VS_COMMENT,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          KC_VOLU,  KC_MUTE,\
 		    KC_TRNS,       KC_TRNS,  KC_TRNS,                  KC_MPLY,                   MO(2),          KC_TRNS,  KC_MPRV,          KC_VOLD,  KC_MNXT),
@@ -27,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 			RESET,       KC_F13,    KC_F14,   KC_F15,  KC_F16,   KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,   KC_F22,   KC_F23,  KC_F24,  RGB_HUD,   RGB_TOG,\
 		    KC_TRNS,       RGB_TOG,  RGB_MOD, RGB_RMOD,RGB_HUI, RGB_HUD, RGB_SAD, RGB_SPI, RGB_SPD, KC_TRNS, KC_PSCR, KC_SLCK,  KC_PAUS, RESET,    RGB_MOD,\
 			CTL_T(KC_CAPS),RGB_SPI,  RGB_SPD, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          EEP_RST,  RGB_RMOD,\
-		    KC_LSFT,       KC_TRNS,  KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          RGB_VAI,  KC_MUTE,\
+		    KC_LSFT,       KC_TRNS,  KC_TRNS, RUN_VSCODE,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,          RGB_VAI,  KC_MUTE,\
 		    KC_TRNS,       KC_TRNS,  KC_TRNS,                  KC_TRNS,                   KC_TRNS,          KC_TRNS,  RGB_SAD,          RGB_VAD,  RGB_SAI),
 };
 
@@ -46,7 +52,13 @@ void matrix_init_user(void)
 
 void matrix_scan_user(void)
 {
-  //user matrix
+  if (windows_run) {
+    if (timer_elapsed(timer) > 500) {
+      windows_run = false;
+      SEND_STRING(code);
+      timer = 0;
+    }
+  }
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record)
@@ -59,20 +71,33 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record)
 		    break;
         case VS_COMMENT:
         	if (record->event.pressed){
-			SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_K)SS_TAP(X_C)SS_UP(X_LCTRL));
+			    SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_K)SS_TAP(X_C)SS_UP(X_LCTRL));
 		    }
+            return false;
             break;
         case VS_UNCOMMENT:
         	if (record->event.pressed){
-			SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_K)SS_TAP(X_U)SS_UP(X_LCTRL));
+			    SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_K)SS_TAP(X_U)SS_UP(X_LCTRL));
 		    }
 		    break;
         case VS_EXTRACTMETHOD:
             if (record->event.pressed){
-			SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_R)SS_TAP(X_M)SS_UP(X_LCTRL));
+			    SEND_STRING(SS_DOWN(X_LCTRL)SS_TAP(X_R)SS_TAP(X_M)SS_UP(X_LCTRL));
 		    }
             break;
+        case RUN_VSCODE:
+            if (record->event.pressed){
+			    SEND_STRING(SS_LGUI(SS_TAP(X_R)));
+                windows_run = true;
+		    }
+            break;
+        case CLEAR:
+            if (record->event.pressed){
+                clear_keyboard();
+            }
+		break;
 	}
 
 	  return true;
 }
+
